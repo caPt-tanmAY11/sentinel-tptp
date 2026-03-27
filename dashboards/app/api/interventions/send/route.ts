3fa9d25e8a502244c89017d9a588c2e869da0f02
Tanmay Vishwakarma <tanmay06lko@gmail.com>
2026-03-27 19:35:49 +0530

    solve merge issues

--- dashboards/app/api/interventions/send/route.ts
+++ dashboards/app/api/interventions/send/route.ts
@@ -5,8 +5,8 @@ import nodemailer from 'nodemailer';
 import { sentinelApi } from '@/lib/api';
 import { randomUUID } from 'crypto';
 
-const SENDER_EMAIL = 'testuser1togethr@gmail.com';
-const RECEIVER_EMAIL = 'tanmay.vishwakarma24@spit.ac.in';
+const SENDER_EMAIL = process.env.SENDER_EMAIL || 'your_new_gmail@gmail.com';
+const RECEIVER_EMAIL = process.env.RECEIVER_EMAIL || 'your_new_gmail@gmail.com';
 
 let cachedTransporter: any = null;
 
